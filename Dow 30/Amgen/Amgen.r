695adae56631e437967a24599d3b60f52e781b34
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-07-12 09:57:18 -0400

    chart for Amgen

---
+++ Dow 30/Amgen/Amgen.r
@@ -0,0 +1,23 @@
+install.packages("magrittr")
+library(plotly)
+
+# x axis
+x = list("Current Share Price", "Upside", "Intrinsic Value")
+measure = c("relative", "relative", "total")
+text = c("", "+247.55", "496.03")
+y = c(248.48, 247.55, 496.03)
+data = data.frame(x=factor(x,levels=x),measure,text,y)
+
+fig <- plot_ly(
+  data, name = "", type = "waterfall", measure = ~measure,
+  x = ~x, textposition = "outside", y= ~y, text =~ text,
+  connector = list(line = list(color = "rgb(63, 63, 63)")))
+fig <- fig %>%
+  layout(title = "Apple Intrinsic Value",
+         xaxis = list(title = ""),
+         yaxis = list(title = "$"),
+         autosize = TRUE,
+         showLegend = FALSE)
+
+fig
+
