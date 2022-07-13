fd1b9779b49f8af037c3509a0610cd7d4b3a364a
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-07-13 12:27:07 -0400

    chart for cisc

---
+++ Dow 30/Cisco/Cisco.R
@@ -0,0 +1,22 @@
+install.packages("magrittr")
+library(plotly)
+
+# x axis
+x = list("Current Share Price", "Upside", "Intrinsic Value")
+measure = c("relative", "relative", "total")
+text = c("42.86", "+111.75", "154.61")
+y = c(42.86, 111.75, 154.61)
+data = data.frame(x=factor(x,levels=x),measure,text,y)
+
+fig <- plot_ly(
+  data, name = "", type = "waterfall", measure = ~measure,
+  x = ~x, textposition = "outside", y= ~y, text =~ text,
+  connector = list(line = list(color = "rgb(63, 63, 63)")))
+fig <- fig %>%
+  layout(title = "Cisco Intrinsic Value",
+         xaxis = list(title = ""),
+         yaxis = list(title = "$"),
+         autosize = TRUE,
+         showLegend = FALSE)
+
+fig
