9fae08205d51ce2f253450d4f9c955d82ebe7201
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-07-19 22:53:09 -0400

    updating values for Apple

--- Dow 30/Apple/Apple.R
+++ Dow 30/Apple/Apple.R
@@ -1,11 +1,10 @@
 install.packages("magrittr")
 library(plotly)
 
-# values to change
 x = list("Current Share Price", "Upside", "Intrinsic Value")
 measure = c("relative", "relative", "total")
-text = c("", "+91.63", "241.70")
-y = c(147.07, 91.63, 241.70)
+text = c("", "+90.7", "241.70")
+y = c(151, 90.7, 241.70)
 data = data.frame(x=factor(x,levels=x),measure,text,y)
 
 fig <- plot_ly(
