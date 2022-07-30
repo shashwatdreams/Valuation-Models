7563340b2dd72edfe3de17468f98d7d75e80d301
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-07-30 19:08:16 -0400

    updating values

--- Dow 30/Cisco/Cisco.R
+++ Dow 30/Cisco/Cisco.R
@@ -3,9 +3,9 @@ library(plotly)
 
 x = list("Current Share Price", "Upside", "Intrinsic Value")
 measure = c("relative", "relative", "total")
-text = c("44.64", "+109.97", "154.61")
+text = c("45.37", "+109.24", "154.61")
 
-y = c(44.64, 109.97, 154.61)
+y = c(45.37, 109.24, 154.61)
 data = data.frame(x=factor(x,levels=x),measure,text,y)
 
 fig <- plot_ly(
