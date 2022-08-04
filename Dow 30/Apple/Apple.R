7eed0a66617ce7a54d3dcd26f02e6c3d3d83889b
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-08-04 17:01:51 -0400

    updating values

--- Dow 30/Apple/Apple.R
+++ Dow 30/Apple/Apple.R
@@ -6,7 +6,7 @@ library(plotly)
 x = list("Current Share Price", "Upside", "Intrinsic Value")
 measure = c("relative", "relative", "total")
 text = c("", "+13.36", "179.49")
-y = c(166.13, 13.36, 179.49)
+y = c(165.81, 13.36, 179.49)
 data = data.frame(x=factor(x,levels=x),measure,text,y)
 
 fig <- plot_ly(
