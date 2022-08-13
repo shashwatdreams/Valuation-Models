94d29f07d90f1866116c3b68a586888370ca6cea
Shashwat Sharma <68554481+shashwat73@users.noreply.github.com>
2022-08-13 00:06:55 -0400

    updating values

--- Dow 30/Amgen/Amgen.r
+++ Dow 30/Amgen/Amgen.r
@@ -8,7 +8,6 @@ text = c("", "+255.04", "506.18")
 y = c(248.35, 255.04, 506.18)
 data = data.frame(x=factor(x,levels=x),measure,text,y)
 
-
 fig <- plot_ly(
   data, name = "", type = "waterfall", measure = ~measure,
   x = ~x, textposition = "outside", y= ~y, text =~ text,
@@ -19,5 +18,6 @@ fig <- fig %>%
          yaxis = list(title = "$"),
          autosize = TRUE,
          showLegend = FALSE)
+
 
 fig
